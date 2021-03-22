b6379d44562711d1718cfde7d418e96a490776c8
Luan Schons <luanschons2000@gmail.com>
2021-03-21 23:11:53 -0300

    Update

--- app/controllers/Web.php
+++ app/controllers/Web.php
@@ -6,51 +6,18 @@
 
 namespace App\Controllers;
 
-use App\Database\Models\Users;
 
 class Web  extends BaseController
 {
 
-    private $users;
-
     public function __construct()
     {
-        $this->users = new Users();
+        return true;
     }
 
     public function home($response)
     {
-
-        $this->mailSend('Teste', 'Testando', 'luanschons2000@gmail.com', 'Luan Schons Griebler');
-        
-        $data = [
-            "router" => $response,
-            "page" => ['title' => 'Home', 'description' => "Página Home."]
-        ];
-
-        return $this->setView('home', $data);
-    }
-
-    public function contact($response)
-    {
-        $data = [
-            "router" => $response,
-            "page" => ['title' => 'Error', 'description' => "Página contato"]
-        ];
-
-        return $this->setView('contact', $data);
-    }
-
-    public function about($response)
-    {
-
-        $data = [
-            "router" => $response,
-            "page" => ['title' => 'About', 'description' => "Página de sobre."]
-        ];
-
-        return $this->setView('about', $data);
-
+        echo 'Testing';
     }
 
     public function error($response)
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInitb1cd1b0ce4f7c192da1018b39bcf30f0
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInitb1cd1b0ce4f7c192da1018b39bcf30f0', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitb1cd1b0ce4f7c192da1018b39bcf30f0', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
